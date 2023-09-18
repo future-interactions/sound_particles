57554ef91711a5aefbbbfd6169db94f068d51509
Matt Wade <matt@Matts-MBP.fritz.box>
2023-09-18 17:16:15 +0100

    first push

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let mic, fft;
+
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  noFill();
+
+  mic = new p5.AudioIn();
+  mic.start();
+  fft = new p5.FFT();
+  fft.setInput(mic);
+strokeWeight(2);
+//background(0);
+}
+
+function draw() {
+	noStroke();
+	fill(0);
+rect(0,0,displayWidth,displayHeight);
+  let spectrum = fft.analyze();
+
+ // beginShape();
+  for (i = 0; i < spectrum.length; i++) {
+	if(spectrum[i]>0){
+		stroke(175-spectrum[i]);
+    //stroke(255);
+	}else{
+		stroke(0);
+	}
+	let x = displayWidth/spectrum.length*i-100;
+	point(x*1.5,map(spectrum[i], 0, 255, height/2, 0));
+	point(x*1.5,map(spectrum[i]*-1, 0, 255, height/2, 0));
+
+   // vertex(i, map(spectrum[i], 0, 255, height/2, 0));
+  }
+ // endShape();
+}
